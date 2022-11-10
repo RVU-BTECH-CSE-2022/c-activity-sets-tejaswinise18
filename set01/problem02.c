b5329fdfee279a60f4407f6b8881eab49dd5cd33
Tejaswini  S <115769533+tejaswinise18@users.noreply.github.com>
2022-11-10 09:34:57 +0000

    problem03 completed

---
+++ set01/problem02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ main()
+{
+  int a = 100;
+  int  b = 250;
+  int sum;
+  sum = a+b;
+  printf("%d\n" ,sum);
+}
---
+++ set01/problem03.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input();
+int add(int a, int b);
+void output(int a,int b,int sum);
+int main()
+  {
+    int a, b, sum;
+    printf("enter two no's: ");
+    a = input();
+    b = input();
+    sum = add(a, b);
+    output(a,b,sum);
+  }
+  int input()
+  {
+    int a;
+    scanf("%d", &a);
+    return a;
+    }
+  
+  int add(int a, int b){
+    int sum;
+    sum = a+b;
+    return sum;
+  }
+  void output(int a, int b, int sum){
+   printf("the sum of %d and %d is %d\n",a,b,sum);
+    }
